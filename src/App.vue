d14383065654080cb69ba0678e6f86072f2e5ee6
dev <dev@devs-MacBook-Pro.local>
2020-08-26 17:43:05 +0300

    Header added

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
+    <Header />
     <Todos v-bind:todos="todos" v-on:delete-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
+import Header from "./components/layout/Header"
 import Todos from "./components/Todos";
 export default {
   name: "App",
-  components: { Todos },
+  components: { Header, Todos },
   data() {
     return {
       todos: [
@@ -21,6 +23,9 @@ export default {
   methods: {
     deleteTodo(id) {
       this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo){
+      this.todos = [...this.todos, newTodo];
     }
   }
 };
@@ -30,11 +35,12 @@ export default {
 * {
   box-sizing: border-box;
   margin: 0;
-  padding: 10px;
+  padding: 0;
 }
 body {
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   line-height: 1.4;
+  padding: 50px 300px;
 }
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="header">
+<h1>TODO LIST</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+    .header {
+        background: #333;
+        color: white;
+        text-align: center;
+        padding: 10px;
+    }
+
+    .header a {
+        color: white;
+        padding-right: 5px;
+    }
+</style>
